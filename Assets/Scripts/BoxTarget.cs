594a0ad6f3154ad8d8bf19b5171009d06e83f8cb
JimW <jwallace@corp.versionone.net>
2016-09-18 17:51:17 -0400

    Add detonator and implemented boxtarget

---
+++ Assets/Scripts/BoxTarget.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxTarget : MonoBehaviour
+{
+	private AudioSource audioSource = null;
+
+	// Use this for initialization
+	void Start()
+	{
+		audioSource = gameObject.AddComponent<AudioSource>();
+		audioSource.playOnAwake = false;
+		audioSource.spatialize = true;
+		audioSource.spatialBlend = 1.0f;
+		audioSource.dopplerLevel = 1.0f;
+		audioSource.loop = false;
+		audioSource.clip = Resources.Load<AudioClip>("blockbreak");
+
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		audioSource.Play();
+		var detonator = GetComponent<Detonator>();
+		if(detonator != null)
+			detonator.Explode();
+		Destroy(collision.gameObject);
+
+	}
+}
